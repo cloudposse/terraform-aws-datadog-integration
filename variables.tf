74f21a02002677a4b2cd895e336f1dc0b21ebd29
Vadim Hleif <vadzim.hleif@gmail.com>
2018-07-17 11:57:35 +0300

    Migrate readme yaml (#3)
    * Migrate readme yaml

--- variables.tf
+++ variables.tf
@@ -1,29 +1,42 @@
-variable "namespace" {}
+variable "name" {
+  description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
+}
 
-variable "stage" {}
+variable "namespace" {
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
 
-variable "name" {}
+variable "stage" {
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
 
 variable "delimiter" {
-  default = "-"
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
 }
 
 variable "attributes" {
-  type    = "list"
-  default = []
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
 }
 
 variable "tags" {
-  type = "map"
-  default = {}
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`)"
 }
 
-variable "datadog_external_id" {}
+variable "datadog_external_id" {
+  description = "External Id of the DataDog service"
+}
 
 variable "datadog_aws_account_id" {
-  default = "464622532012"
+  description = "Datadog’s AWS account ID"
 }
 
 variable "integrations" {
-  type = "list"
+  type        = "list"
+  description = "List of AWS Services to integration with the DataDog service (e.g EC2, RDS, Billing ...)"
 }
