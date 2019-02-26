1c1f99a0c13f3feeb0285254e62f3a7d84c82feb
Jeremy Grodberg <Nuru@users.noreply.github.com>
2019-02-25 22:23:45 -0800

    Remove unused variables

--- variables.tf
+++ variables.tf
@@ -11,22 +11,10 @@ variable "stage" {
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
-variable "delimiter" {
-  type        = "string"
-  default     = "-"
-  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
-}
-
 variable "attributes" {
   type        = "list"
   default     = []
   description = "Additional attributes (e.g. `policy` or `role`)"
-}
-
-variable "tags" {
-  type        = "map"
-  default     = {}
-  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`)"
 }
 
 variable "datadog_external_id" {
