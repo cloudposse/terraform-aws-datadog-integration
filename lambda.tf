968e401d02b5e3ab1f820929d6d3c80fb2164e6c
Steven Hopkins <steve@cloudposse.com>
2021-05-12 14:17:03 -0700

    Refactor variable validation, add enable flag and use module.this

--- lambda.tf
+++ lambda.tf
@@ -2,12 +2,11 @@
 ## https://docs.datadoghq.com/integrations/amazon_rds/?tab=enhanced
 ##
 ## TODO 
-#   - Add some type of enabling feature flag for rds enahnced metrics
-#   - Move variables to variables.tf
+#   - Move variables to variables.tf???
 #   - Watch https://github.com/hashicorp/terraform/issues/15469 for future validation/error checking (variable validation can currently only reference itself)
 
 variable "subnet_ids" {
-  description = "List of subnets to use when running in specific VPC"
+  description = "List of subnets to use when running in specific VPC."
   type        = list(string)
   default     = null
 }
@@ -18,6 +17,12 @@ variable "security_group_ids" {
   default     = null
 }
 
+variable "lambda_runtime" {
+  type        = string
+  description = "Runtime environment for Datadog lambda"
+  default     = "python3.7"
+}
+
 variable "tracing_config_mode" {
   type        = string
   description = "Can be either PassThrough or Active. If PassThrough, Lambda will only trace the request from an upstream service if it contains a tracing header with 'sampled=1'. If Active, Lambda will respect any tracing header it receives from an upstream service."
@@ -25,27 +30,54 @@ variable "tracing_config_mode" {
 }
 
 variable "dd_api_key_source" {
-  description = "ARN (kms or asm) or parameter name (for ssm) to retrieve Datadog api key."
+  description = "ARN for retrieving api key. Datadog lambda allows for only one of (kms, asm, ssm) secrets management."
   type = object({
-    kms = string # kms key arn
-    asm = string # asm secret arn
-    ssm = string # paramater name
+    resource   = string
+    identifier = string
   })
 
   default = {
-    kms = ""
-    asm = ""
-    ssm = ""
+    resource   = ""
+    identifier = ""
   }
 
+  # Resource can be one of kms, asm, ssm ("" to disable all lambda resources)
   validation {
-    condition     = length(compact(values(var.dd_api_key_source))) == 1
-    error_message = "Provide only one ARN (kms or asm) or parameter name (for ssm) to retrieve Datadog api key."
+    condition     = can(regex("(kms|asm|ssm)", var.dd_api_key_source.resource)) || var.dd_api_key_source.resource == ""
+    error_message = "Provide one, and only one, ARN for (kms, asm) or name (ssm) to retrieve or decrypt Datadog api key."
+  }
+
+  # Check kms arn format
+  validation {
+    condition     = var.dd_api_key_source.resource == "kms" ? can(regex("arn:aws:kms:.*:key/.*", var.dd_api_key_source.identifier)) : true
+    error_message = "ARN for KMS key does not appear to be valid format (example: arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab)."
+  }
+
+  # Check asm arn format 
+  validation {
+    condition     = var.dd_api_key_source.resource == "asm" ? can(regex("arn:aws:secretsmanager:.*:secret:.*", var.dd_api_key_source.identifier)) : true
+    error_message = "ARN for AWS Secrets Manager (asm) does not appear to be valid format (example: arn:aws:secretsmanager:us-west-2:111122223333:secret:aes128-1a2b3c)."
+  }
+
+  # Check ssm name format
+  validation {
+    condition     = var.dd_api_key_source.resource == "ssm" ? can(regex("^[a-zA-Z0-9_./-]+$", var.dd_api_key_source.identifier)) : true
+    error_message = "Name for SSM parameter does not appear to be valid format, acceptable characters are a-zA-Z0-9_.- and / to delineate hierarchies."
   }
 }
 
 variable "dd_api_key_kms_ciphertext_blob" {
-  default = ""
+  type        = string
+  description = "CiphertextBlob stored in enironment variable DD_KMS_API_KEY used by the lambda function, along with the kms key, to decrypt Datadog API key."
+  default     = ""
+}
+
+locals {
+  lambda_enable          = var.dd_api_key_source.resource != "" ? true : false
+  dd_api_key_resource    = var.dd_api_key_source.resource
+  dd_api_key_identifier  = var.dd_api_key_source.identifier
+  dd_api_key_arn         = local.dd_api_key_resource == "ssm" ? data.aws_ssm_parameter.api_key[0].arn : local.dd_api_key_identifier
+  dd_api_key_iam_actions = [lookup({ kms = "kms:Decrypt", asm = "secretsmanager:GetSecretValue", ssm = "ssm:GetParameters" }, local.dd_api_key_resource, "")]
 }
 
 data "aws_ssm_parameter" "api_key" {
@@ -53,21 +85,12 @@ data "aws_ssm_parameter" "api_key" {
   name  = local.dd_api_key_identifier
 }
 
-locals {
-  dd_api_key_source = {
-    for k, v in var.dd_api_key_source :
-    k => v if v != ""
-  }
-  dd_api_key_resource    = keys(local.dd_api_key_source)[0]
-  dd_api_key_identifier  = values(local.dd_api_key_source)[0]
-  dd_api_key_arn         = local.dd_api_key_resource == "ssm" ? data.aws_ssm_parameter.api_key[0].arn : local.dd_api_key_identifier
-  dd_api_key_iam_actions = [lookup({ kms = "kms:Decrypt", asm = "secretsmanager:GetSecretValue", ssm = "ssm:GetParameters" }, local.dd_api_key_resource, "")]
-}
-
 ######################################################################
 ## Create base assume policy and lambda role
 
 data "aws_iam_policy_document" "assume" {
+  count = local.lambda_enable == true ? 1 : 0
+
   statement {
     effect = "Allow"
 
@@ -83,17 +106,18 @@ data "aws_iam_policy_document" "assume" {
 }
 
 resource "aws_iam_role" "lambda" {
-  name               = "lambda" # module.this.id
-  assume_role_policy = data.aws_iam_policy_document.assume.json
-  # tags               = module.this.tags
+  count = local.lambda_enable == true ? 1 : 0
+
+  name               = module.this.id
+  assume_role_policy = data.aws_iam_policy_document.assume[0].json
+  tags               = module.this.tags
 }
 
 ######################################################################
 ## Create lambda logging and secret policy then attach to base lambda role
 
-# TODO review DD specific reqs 
-# https://github.com/DataDog/datadog-serverless-functions/blob/cb0e7965636a3ea613325d2d4624926600a436c0/aws/logs_monitoring/template.yaml
 data "aws_iam_policy_document" "lambda" {
+  count = local.lambda_enable == true ? 1 : 0
 
   statement {
     sid = "WriteLogs"
@@ -104,12 +128,6 @@ data "aws_iam_policy_document" "lambda" {
       "logs:CreateLogGroup",
       "logs:CreateLogStream",
       "logs:PutLogEvents",
-      # I think these are only needed for other DD lambda forwarder types 
-      # https://docs.aws.amazon.com/lambda/latest/dg/configuration-vpc.html#vpc-permissions
-      # https://github.com/DataDog/datadog-serverless-functions/blob/cb0e7965636a3ea613325d2d4624926600a436c0/aws/logs_monitoring/template.yaml#L553-L560
-      # "ec2:CreateNetworkInterface",
-      # "ec2:DescribeNetworkInterfaces",
-      # "ec2:DeleteNetworkInterface"
     ]
 
     resources = ["*"]
@@ -128,33 +146,45 @@ data "aws_iam_policy_document" "lambda" {
 }
 
 resource "aws_iam_policy" "lambda" {
-  name        = "lambda" # module.this.id
+  count = local.lambda_enable == true ? 1 : 0
+
+  name        = module.this.id
   description = "Allow put logs and access to DD api key"
-  policy      = data.aws_iam_policy_document.lambda.json
+  policy      = data.aws_iam_policy_document.lambda[0].json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda" {
-  role       = aws_iam_role.lambda.name
-  policy_arn = aws_iam_policy.lambda.arn
+  count = local.lambda_enable == true ? 1 : 0
+
+  role       = aws_iam_role.lambda[0].name
+  policy_arn = aws_iam_policy.lambda[0].arn
 }
 
 ######################################################################
 ## Get DD lambda zip artifact
 ## https://github.com/DataDog/datadog-serverless-functions/releases
-## TODO move variables to variables.tf
+## TODO move variables to variables.tf???
 
 variable "dd_artifact_filename" {
-  default = "aws-dd-forwarder"
+  type        = string
+  description = "The datadog artifact filename minus extension."
+  default     = "aws-dd-forwarder"
 }
 
 variable "dd_module_name" {
-  default = "datadog-serverless-functions"
+  type        = string
+  description = "The Datadog github repository name."
+  default     = "datadog-serverless-functions"
 }
 variable "dd_git_ref" {
-  default = "3.31.0"
+  type        = string
+  description = "The version of the Datadog artifact zip file."
+  default     = "3.31.0"
 }
 
 variable "dd_artifact_url" {
+  type        = string
+  description = "The url template to format the full url to the Datadog zip artifact."
   # I don't like mixing format with template, I also don't want to create too much nesting in template string which might cause some to miss it if the modify it
   default = "https://github.com/DataDog/$$${module_name}/releases/download/%v-$$${git_ref}/$$${filename}"
 }
@@ -165,6 +195,8 @@ locals {
 }
 
 module "artifact" {
+  count = local.lambda_enable == true ? 1 : 0
+
   source      = "cloudposse/module-artifact/external"
   version     = "0.7.0"
   filename    = local.filename
@@ -186,14 +218,16 @@ locals {
 }
 
 resource "aws_lambda_function" "default" {
+  count = local.lambda_enable == true ? 1 : 0
+
   description      = "Datadog forwarder for RDS enhanced monitoring"
-  filename         = module.artifact.file
-  function_name    = "datadog" # module.this.id
-  role             = aws_iam_role.lambda.arn
+  filename         = module.artifact[0].file
+  function_name    = module.this.id
+  role             = aws_iam_role.lambda[0].arn
   handler          = "lambda_function.lambda_handler"
-  source_code_hash = module.artifact.base64sha256
-  runtime          = "python3.7" # var.lambda_runtime
-  # tags             = module.this.tags
+  source_code_hash = module.artifact[0].base64sha256
+  runtime          = var.lambda_runtime
+  tags             = module.this.tags
 
   dynamic "vpc_config" {
     for_each = var.subnet_ids != null && var.security_group_ids != null ? [true] : []
@@ -212,10 +246,10 @@ resource "aws_lambda_function" "default" {
   }
 }
 
-output "lambda_env" {
-  value = local.lambda_env
+output "lambda_function_id" {
+  value = length(aws_lambda_function.default) > 0 ? aws_lambda_function.default[0].id : null
 }
 
-output "aws_iam_policy_document" {
-  value = data.aws_iam_policy_document.lambda.json
+output "lambda_iam_role_id" {
+  value = length(aws_iam_role.lambda) > 0 ? aws_iam_role.lambda[0].id : null
 }
