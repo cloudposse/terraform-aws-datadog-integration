9b360aa1a111dc89299515a9e3d98878e0b37214
Steven Hopkins <steve@cloudposse.com>
2021-06-28 12:11:19 -0700

    fixing concat

--- lambda.tf
+++ lambda.tf
@@ -124,7 +124,7 @@ module "artifact" {
 
 data "external" "curl" {
   # count      = module.this.enabled ? 1 : 0
-  program = concat(["curl"], "-fsSL", ["--write-out", "{\"success\": \"true\", \"filename_effective\": \"lambda.py\"}", "-o", "lambda.py", "https://raw.githubusercontent.com/DataDog/datadog-serverless-functions/master/aws/rds_enhanced_monitoring/lambda_function.py"])
+  program = concat(["curl"], ["-fsSL"], ["--write-out", "{\"success\": \"true\", \"filename_effective\": \"lambda.py\"}", "-o", "lambda.py", "https://raw.githubusercontent.com/DataDog/datadog-serverless-functions/master/aws/rds_enhanced_monitoring/lambda_function.py"])
   # depends_on = [data.external.git]
 }
 
@@ -133,7 +133,7 @@ data "archive_file" "init" {
   source_file = "${path.module}/lambda.py"
   output_path = "${path.module}/lambda.zip"
   depends_on = [
-    "data.external.curl"
+    data.external.curl
   ]
 }
 
@@ -172,7 +172,7 @@ resource "aws_lambda_function" "default" {
   }
 
   depends_on = [
-    "data.archive_file.init"
+    data.archive_file.init
   ]
 }
 
