e28742462316f6d9527815fcf1f0ad4e65275bd3
nitrocode <nitrocode@users.noreply.github.com>
2021-11-24 18:32:35 -0600

    Bump providers

--- versions.tf
+++ versions.tf
@@ -4,15 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
+      version = ">= 3.0"
     }
     datadog = {
       source  = "datadog/datadog"
-      version = ">= 2.13"
+      version = ">= 3.0"
     }
   }
 }
